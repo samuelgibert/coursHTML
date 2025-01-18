33cf334939b54a6c62968b06be7df9453098af51
perso <perso@DESKTOP-VN9OFRR.lan>
2025-01-18 18:44:19 +0100

    sommaire + lien vers code pen

--- public_html/script.js
+++ public_html/script.js
@@ -53,6 +53,6 @@ function generateToc(containerId, headers) {
         list.appendChild(listItem);
     });
 
-    toc.innerHTML = '<strong>Sommaire :</strong>';
+    toc.innerHTML = '<a href="/index.html" > <h4>&#8962; go home</h3> </a><br><strong>Sommaire :</strong>';
     toc.appendChild(list);
 }
