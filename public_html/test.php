82e9b74235857856aec5fdf98003baf2fbc9bbc9
perso <perso@DESKTOP-VN9OFRR.lan>
2025-01-21 14:52:41 +0100

    test php

---
+++ public_html/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours HTML CSS JS</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="monCSS.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body class="p">
+        <a href="cours_1_html.html">cours 1 HTML</a>
+        <a href="cours_2_html.html">cours 2 HTML</a>
+        
+        <?php echo("coucou"); ?>
+    </body>
+</html>
